47430acd78f4b326e9723431ec8d67304d65f3f6
juanmoralesmolina4-eng <juanmoralesmolina4@gmail.com>
2026-01-22 00:24:13 +0100

    fix: synchronize state properties between state.js and generator.js (inventory type, add temp_hp, notes, feats, align)

--- app/static/js/00_CORE/core/state.js
+++ app/static/js/00_CORE/core/state.js
@@ -7,16 +7,18 @@ export const state = {
     classes: [],
     stats: { str: 10, dex: 10, con: 10, int: 10, wis: 10, cha: 10 },
     hp_curr: 10,
+    temp_hp: 0,
     skills: {},
     weapons: [],
     spells: [],
     buffs: [],
-    feats: [], // New Feats Array
+    feats: [],
     inventory: "",
     gold: 0,
-    ac_manual: { armor: 0, shield: 0, natural: 0, deflect: 0 },
+    ac_manual: { armor: 0, shield: 0, natural: 0, deflect: 0, dodge: 0, misc: 0 },
     acp_manual: 0,
-    used_slots: {} // Spell slots usage
+    used_slots: {},
+    notes: ""
 };
 
 export const globalFlags = {
--- app/static/js/01_MODULOS/features/archetypes/generator.js
+++ app/static/js/01_MODULOS/features/archetypes/generator.js
@@ -77,6 +77,7 @@ function generateCharacter(cls, lvl) {
     const newState = {
         name: `${cls} Nivel ${lvl}`,
         race: "Humano",
+        align: "Neutral",
         classes: [{ id: cls, lvl: lvl }],
         stats: { str: 10, dex: 10, con: 10, int: 10, wis: 10, cha: 10 },
         hp_curr: 10,
@@ -85,9 +86,10 @@ function generateCharacter(cls, lvl) {
         acp_manual: 0,
         skills: {},
         weapons: [],
-        inventory: [],
+        inventory: "",
         spells: [],
         buffs: [],
+        feats: [],
         used_slots: {},
         gold: 0,
         notes: ""
@@ -125,9 +127,8 @@ function generateCharacter(cls, lvl) {
     tiers.forEach(t => { if (lvl >= t) bestTier = t; });
 
     if (ark.gear && ark.gear[bestTier]) {
-        ark.gear[bestTier].forEach(item => {
-            state.inventory.push({ n: item, w: 0, q: 1 });
-        });
+        const gearList = ark.gear[bestTier].join(', ');
+        state.inventory = gearList;
     }
 
     // 5. Spells
